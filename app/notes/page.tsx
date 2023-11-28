989bf6cce1d976e68dd24afee86e6d5e399152e6
Mateo Sam <msam@the_port.com>
2023-11-28 07:21:39 -0400

    reordered notes and changed first page to direct to login if not logged in

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -9,62 +9,66 @@ import { toast } from "react-toastify";
 import Notecard from "@/components/notes/card/Notecard";
 
 export default function Page() {
-	const [notes, setNotes] = useState<any[] | null>(null);
-	const [loading, setLoading] = useState<boolean>(true);
-	const supabase = createClient();
-	const [user, setUser] = useState<any | null>(null);
+  const [notes, setNotes] = useState<any[] | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const supabase = createClient();
+  const [user, setUser] = useState<any | null>(null);
 
-	const getUser = async () => {
-		const {
-			data: { user },
-		} = await supabase.auth.getUser();
-		setUser(user);
-	};
-	useEffect(() => {
-		const getData = async () => {
-			const { data, error } = await supabase
-				.from("notes")
-				.select(`*, note_users(email), comments(comment)`)
-				.eq("deleted", false);
-			if (error) {
-				console.log("Error");
-				console.log(error);
-				toast.error("Error fetching notes.");
-				setLoading(false);
+  const getUser = async () => {
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+    setUser(user);
+  };
+  useEffect(() => {
+    const getData = async () => {
+      const { data, error } = await supabase
+        .from("notes")
+        .select(`*, note_users(email), comments(comment)`)
+        .eq("deleted", false);
+      if (error) {
+        console.log("Error");
+        console.log(error);
+        toast.error("Error fetching notes.");
+        setLoading(false);
 
-				return;
-			}
-			console.log(data);
-			setNotes(data);
-			setLoading(false);
-		};
-		getUser();
+        return;
+      }
+      console.log(data);
+      setNotes(data);
+      setLoading(false);
+    };
+    getUser();
 
-		getData();
-	}, []);
-	if (loading) {
-		return (
-			<div className="flex justify-center items-center h-screen">
-				Loading notes...
-			</div>
-		);
-	}
-	return (
-		<div className="w-[90vw] max-w-[600px] mx-auto flex flex-col justify-center">
-			<div className="mb-10 mt-3 border-b border-foreground flex justify-between items-center">
-				<h2 className="text-2xl">Notes</h2>
+    getData();
+  }, []);
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        Loading notes...
+      </div>
+    );
+  }
+  return (
+    <div className="w-[90vw] max-w-[600px] mx-auto flex flex-col justify-center">
+      <div className="mb-10 mt-3 border-b border-foreground flex justify-between items-center">
+        <h2 className="text-2xl">Notes</h2>
 
-				<Link href={"/newnote"}>
-					<IoMdAdd className="text-3xl" />
-				</Link>
-			</div>
-			{notes ? (
-				notes.map((note) => <Notecard key={note.id} note={note} user={user} />)
-			) : (
-				<div className="w-full flex justify-center items-center  ">
-					<p className="px-4 py-2 rounded">No notes</p>
-				</div>
-			)}
-		</div>
-	);
+        <Link href={"/newnote"}>
+          <IoMdAdd className="text-3xl" />
+        </Link>
+      </div>
+      {notes ? (
+        <div className="flex flex-col-reverse">
+          {notes.map((note) => (
+            <Notecard key={note.id} note={note} user={user} />
+          ))}
+        </div>
+      ) : (
+        <div className="w-full flex justify-center items-center  ">
+          <p className="px-4 py-2 rounded">No notes</p>
+        </div>
+      )}
+    </div>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,32 @@
 import Link from "next/link";
+import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
 
 export default async function Index() {
+  const cookieStore = cookies();
+
+  const supabase = createClient(cookieStore);
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       <div className=" w-full min-h-[40vh] max-h-screen flex items-center justify-center">
-        <Link
-          href={"/notes"}
-          className="bg-btn-background hover:bg-btn-background-hover transition-all duration-300 px-5 py-2 rounded"
-        >
-          To Notes
-        </Link>
+        {user ? (
+          <Link
+            href={"/notes"}
+            className="bg-btn-background hover:bg-btn-background-hover transition-all duration-300 px-5 py-2 rounded"
+          >
+            To Notes
+          </Link>
+        ) : (
+          <Link
+            href={"/login"}
+            className="bg-btn-background hover:bg-btn-background-hover transition-all duration-300 px-5 py-2 rounded"
+          >
+            To Login
+          </Link>
+        )}
       </div>
 
       <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
